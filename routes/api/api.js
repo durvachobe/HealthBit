dcb015e15f898d41f59e491522f6eabfdc9cc163
Adarsh Shete <shete.adarsh@gmail.com>
2022-08-23 17:12:17 +0530

    backend changes for creating tables

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,27 +1,44 @@
-const express = require('express');
+const express = require("express");
 
 const api = express.Router();
 
-const db = require('../../utils/db');
+const db = require("../../utils/db");
+const {
+  createTable,
+  PATIENT_TABLE_QUERY,
+  DOCTORS_TABLE_QUERY,
+} = require("../../utils/queries");
 
-api.get('/doctors', (req, res) => {
-    const sql = "SELECT first_name, last_name, email, specialisation FROM doctors";
+api.get("/doctors", (req, res) => {
+  createTable(DOCTORS_TABLE_QUERY)
+    .then(() => {
+      const sql =
+        "SELECT first_name, last_name, email, specialisation FROM doctors";
 
-    db.query(sql, (err, result) => {
-        if(err) console.log(err);
+      db.query(sql, (err, result) => {
+        if (err) console.log(err);
 
         res.json(result);
+      });
+    })
+    .catch((err) => {
+      res.status(400).send(err);
     });
 });
 
-api.get('/patients', (req, res) => {
-    const sql = "SELECT first_name, last_name, email, address, phone_no FROM patient";
+api.get("/patients", (req, res) => {
+  createTable(PATIENT_TABLE_QUERY)
+    .then(() => {
+      const sql =
+        "SELECT first_name, last_name, email, address, phone_no FROM patient";
 
-    db.query(sql, (err, result) => {
-        if(err) console.log(err);
+      db.query(sql, (err, result) => {
+        if (err) console.log(err);
 
         res.json(result);
-    });
+      });
+    })
+    .catch((err) => res.status(400).send(err));
 });
 
 // api.get();
